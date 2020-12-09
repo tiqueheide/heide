0219e9432df6b393af133c111e4c01d406f8bbae
tiqueheide <75727784+tiqueheide@users.noreply.github.com>
2020-12-09 17:28:40 +0700

    Add files via upload

---
+++ TIQUE,HEIDE M. SYSTEM/hhshhs.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+
+$c = 0;
+for ($a=1 ; $a<=3 ; $a++)
+{
+	$b = 2 * $a;
+	$c = $c = $b;
+	echo "A = " .$a;
+	echo "B = " .$b;
+	echo "C = " .$c. "<br>";
+
+}
